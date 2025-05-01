1b430ea7908a2f11d41f98d8fdaa749299f4b15b
Seldun <116086201+Seldun@users.noreply.github.com>
2025-05-01 14:56:42 +0300

    Update solution_with_n.py

--- 10/solution_with_n.py
+++ 10/solution_with_n.py
@@ -4,7 +4,7 @@
 for n in range(4,10000):
     s = '5' + n*'2'
 
-    while '72' in s or '522' in s or '2222' in s:
+    while '72' in s or '522' in s or '2222' in s: # очерёдность if очень важна!!! см алоритм в задании
         if '72' in s:
             s = s.replace('72','2',1)
         if '522' in s:
