6918d0a5f60727ccc637e2d0ceb0ba09fa6c2faa
Seldun <116086201+Seldun@users.noreply.github.com>
2025-05-01 09:11:26 +0300

    Add files via upload

---
+++ 10/solution_with_n.py
@@ -0,0 +1,17 @@
+"""
+Это  программа, в которой строка строится с n количеством каких-то символов
+"""
+for n in range(4,10000):
+    s = '5' + n*'2'
+
+    while '72' in s or '522' in s or '2222' in s:
+        if '72' in s:
+            s = s.replace('72','2',1)
+        if '522' in s:
+            s = s.replace('522','27',1)
+        if '2222' in s:
+            s = s.replace('2222','5',1)
+
+    summ = sum(int(i) for i in s)
+    if summ == 63:
+        print(n)
